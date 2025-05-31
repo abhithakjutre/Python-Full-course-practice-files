a671b8507074a35c47a38b152054dff44532b89a
Abhishek Thakur <144336328+abhithakjutre@users.noreply.github.com>
2025-05-30 20:42:32 -0800

    This is the chapter 1 file

---
+++ Python Course Files/CHAPTER 1/module.py
@@ -0,0 +1,10 @@
+import pyjokes #  import module pyjokes
+
+joke = pyjokes.get_joke()
+
+# it is the print function to print anything
+print(joke)
+
+"""it is the print random jokes
+when run the program everytime , 
+and it is the example of the multi line comment"""
